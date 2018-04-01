4c7ce50bfa5fce87c56053787835f16d1538cad3
spokhyan <santc2002@live.com>
2018-04-01 14:23:43 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import data from './data';
+
+console.log(data);
+
+import App from './App';
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
